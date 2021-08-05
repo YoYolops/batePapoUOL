6e29f67f7b4958a39cf60eac4d5bd3ffff4c1495
Yohan Lopes <yohanhxh@gmail.com>
2021-08-05 12:15:30 -0300

    corrigindo bug gerado quando usuarios com usernames com espaços entravam na sala

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ function toggleMenu(event) {
     }
 }
 
+
 /** 
  * Keeps sending connection signals to backend, so it will know the user's still online
  */
@@ -35,6 +36,7 @@ function keepConectionAlive() {
     }, 5000)
 }
 
+
 /** 
  * Gets all the messages sended to the backend every three seconds and sends its data to GLOBAL object
  */
@@ -45,6 +47,7 @@ function getMessages() {
         updateDisplayedMessagesManager();
     }, 3000)
 }
+
 
 /** 
  * Makes a deepest comparation between two objects than "===" operator, wich is a shallow compare
@@ -69,6 +72,7 @@ function areTheseObjectsEqual(objOne, objTwo) {
     return true;
 }
 
+
 /** 
  * Manages the functions that checks the new messages and, if there is any new messages, renders
  * them on screen and updates the apps cache (GLOBAL object).
@@ -84,7 +88,8 @@ function updateDisplayedMessagesManager() {
 
 
 /** 
- * Iterates over the messages received from server and checks wich ones are new
+ * Iterates over the messages received from server and checks wich ones are new, and should be shown,
+ * private messages to other people are not rendered.
  * @return {Array<object>} array of objects that represents the messages data that were not diplayed yet
  */
 function wichMessagesShouldBeAddedInHtml() {
@@ -98,20 +103,56 @@ function wichMessagesShouldBeAddedInHtml() {
     const lastRenderedMessage = GLOBAL.renderedMessages[GLOBAL.renderedMessages.length - 1];
     for(let i=GLOBAL.messages.length-1; i>=0; i--) {
         if(!areTheseObjectsEqual(GLOBAL.messages[i], lastRenderedMessage)) {
-            newMessages.push(GLOBAL.messages[i]);
+            GLOBAL.messages[i].type === "private_message" &&
+            GLOBAL.messages[i].to !== GLOBAL.username &&
+            GLOBAL.messages[i].to !== "Todos" &&
+            GLOBAL.messages[i].from !== GLOBAL.username
+                ? void(0)
+                : newMessages.push(GLOBAL.messages[i]);
+
             GLOBAL.messages[i].type === "status"
                 ? onlineUsersThatShouldBeUpdated.push(GLOBAL.messages[i])
-                : void(0)
+                : void(0);
         } else {
             break;
         }
     }
-    console.log("onlineUsersThatShouldBeUpdated: ")
-    console.log(onlineUsersThatShouldBeUpdated)
     //this approach prevents the need of making another request to /participants end point
     updateOnlineUsersBasedOnStatusMessages(Array.from(onlineUsersThatShouldBeUpdated));
     return newMessages;
 }
+
+
+/** 
+ * creates a HTML string that represents the message.
+ * @param {Object} messageJSON the JSON that comes from the backend, representing a single message data
+ * @return {String} the HTML template string that can be used to display the message on the screen
+ */
+ function generateMessageHTML(messageJSON) {
+    const { from, to, text, type, time } = messageJSON;
+    const messageTemplate = (
+        `<div class="message-box ${type}">
+            <span class="message-time">(${time})</span>
+            <span class="message-from-to"><strong>${from}</strong> para <strong>${to}</strong>:</span>
+            <p class="message-text">${text}</p>
+        </div>`
+    )
+    return messageTemplate;
+}
+
+
+/** 
+ * Append messages on .messages-container
+ */
+function displayMessage(messageHTML) {
+    const messageContainer = document.querySelector(".messages-container");
+    messageContainer.insertAdjacentHTML("beforeend", messageHTML);
+
+    if(window.pageYOffset >= messageContainer.scrollHeight - window.innerHeight - 160) {
+        document.querySelector(".message-box:last-child").scrollIntoView();
+    }
+}
+
 
 /** 
  * Gets all the active users when its called and sends its data to GLOBAL object
@@ -127,18 +168,20 @@ async function getOnlineUsers() {
     }
 }
 
-function generateOnlineUserHtmlTemplate(username) {
+function generateOnlineUserHtmlTemplate(usernameInput) {
+    const username = usernameInput.replace(/\s/g, '') // Erase all blank spaces
     const onlineUserHtmlTemplate = (
-        `<li class="online-user" id="${username}" onclick="changeMessageReceiver(this, event)">
+        `<li class="online-user" id="usr-${username}" onclick="changeMessageReceiver(this, event)">
             <div class="menu-left-section">
                 <img class="menu-left-ico" src="../assets/icos/singleUser.png">
-                <span>${username}</span>
+                <span>${usernameInput}</span>
             </div>
             <img class="menu-right-ico" src="../assets/icos/verify.png">
         </li>`
     )
     return onlineUserHtmlTemplate;
 }
+
 
 /** 
  * Inserts the online users data into DOM, only runs a single time, when app is started.
@@ -152,62 +195,38 @@ function generateOnlineUserHtmlTemplate(username) {
     })
 }
 
+
 /** 
  * Receive the status messages sent from server and, based on them, see wich users should be
  * removed from ul.online-users and wich should be added.
- * @param {Array<object>} messages the array of new status messages
+ * @param {Array<object>} messagesInput the array of new status messages
  */
 function updateOnlineUsersBasedOnStatusMessages(messages) {
     const onlineUsersContainer = document.querySelector(".online-users");
-    messages.forEach(message => {
-        if(message.text === "sai da sala...") {
-            console.log(document.querySelector(`#${message.from}`))
-            document.querySelector(`#${message.from}`).remove();
-        } else if(message.text === "entra na sala..."){
-            const onlineUserHtmlTemplate = generateOnlineUserHtmlTemplate(message.from);
+    console.log(messages)
+    for(let i = 0; i < messages.length; i++) {
+        let usernameFrom = messages[i].from.replace(/\s/g, '');
+        if(messages[i].text === "sai da sala...") {
+            const elementToRemove = document.querySelector(`#usr-${usernameFrom}`)
+            console.log("retirando id:")
+            console.log(`#usr-${usernameFrom}`)
+            elementToRemove.remove();
+        } else if(messages[i].text === "entra na sala..."){
+            const onlineUserHtmlTemplate = generateOnlineUserHtmlTemplate(usernameFrom);
             onlineUsersContainer.insertAdjacentHTML("beforeend", onlineUserHtmlTemplate);
         }
-    })
-}
-
-/** 
- * creates a HTML string that represents the message.
- * @param {Object} messageJSON the JSON that comes from the backend, representing a single message data
- * @return {String} the HTML template string that can be used to display the message on the screen
- */
-function generateMessageHTML(messageJSON) {
-    const { from, to, text, type, time } = messageJSON;
-    const messageTemplate = (
-        `<div class="message-box ${type}">
-            <span class="message-time">(${time})</span>
-            <span class="message-from-to"><strong>${from}</strong> para <strong>${to}</strong>:</span>
-            <p class="message-text">${text}</p>
-        </div>`
-    )
-    return messageTemplate;
-}
-
-/** 
- * Append messages on .messages-container
- */
-function displayMessage(messageHTML) {
-    const messageContainer = document.querySelector(".messages-container");
-    messageContainer.insertAdjacentHTML("beforeend", messageHTML);
-
-    if(window.pageYOffset >= messageContainer.scrollHeight - window.innerHeight - 160) {
-        document.querySelector(".message-box:last-child").scrollIntoView();
     }
 }
-
 
 function sendMessage() {
     const typedMessage = document.querySelector("footer > textarea").value;
     const messageJSON = {
         from: GLOBAL.username,
-        to: GLOBAL.messageConfig.to,
+        to: GLOBAL.messageConfig.to ?? "Todos" ,
         text: typedMessage,
         type: GLOBAL.messageConfig.type
     }
+    console.log(messageJSON)
     GLOBAL.api.post("/messages", messageJSON);
     document.querySelector("footer > textarea").value = "";
 }
@@ -233,7 +252,7 @@ function changeMessageReceiver(element, event) {
     event.stopPropagation();
     unselectAllOnlineUsers();
     element.querySelector(".menu-right-ico").style.visibility = "visible";
-    GLOBAL.messageConfig.to = element.id;
+    GLOBAL.messageConfig.to = element.id.split("-")[1];
 }
 
 function unselectAllOnlineUsers() {
